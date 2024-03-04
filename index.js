8b78b4448712c807f136402f3bfb779f16d10175
mustafakenlic <160403140+mustafakenlic@users.noreply.github.com>
2024-03-04 07:29:06 +0300

    desing

---
+++ index.js
@@ -0,0 +1,14 @@
+//define the DOM objects
+let convertTypeSelect = document.getElementById("convertType");
+let convertNumberInput = document.getElementById("convertNumber");
+let convertButton = document.getElementById("convertBtn");
+
+let lengthTable = document.querySelector("#length table");
+let areaTable = document.querySelector("#area table");
+let volumeTable = document.querySelector("#volume table");
+let weigthTable = document.querySelector("#weigth table");
+
+
+convertButton.click = () =>{
+    console.log("asdf")
+}
